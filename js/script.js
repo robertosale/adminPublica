cbb5fcf6d9ca9be7f141a9665b78d5a96e18bc5c
Roberto Sale <robertito.sale@gmail.com>
2019-11-27 18:57:24 -0300

    almost finished addEmpleadoForm

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function addFormHijos(){
+    let cantidad = document.getElementById("inputHijos").value;
+    console.log(`Tiene ${cantidad} de hijos`);
+
+    document.getElementById("hijos").innerHTML="";
+    cantidad = parseInt(cantidad);
+
+    for(let i=0;i<cantidad;i++) {
+        document.getElementById("hijos").innerHTML+=
+        `<div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="inputNombreHijo${i}">Nombre Hijo ${i+1}</label>
+          <input type="text" class="form-control" id="inputNombreHijo${i}" placeholder="Nombre">
+        </div>
+        <div class="form-group col-md-6">
+          <label for="inputApellidoHijo${i}">Apellido</label>
+          <input type="password" class="form-control" id="inputApellidoHijo${i}" placeholder="Apellido">
+        </div>
+      </div>
+
+      <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="inputDNIHijo${i}">DNI</label>
+                <input type="text" class="form-control" id="inputDNIHijo${i}" placeholder="DNI">
+              </div>
+              
+            </div>`
+    }
+
+}
+
